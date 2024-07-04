6aa8d4ca6e2dbb436a63f33e3b48124ae3614b1e
rngnnn <rngnnnclk@gmail.com>
2024-07-04 12:29:25 +0200

    SASS

--- class-notes/RC-03-react-bootstrap/src/components/MyCard.jsx
+++ class-notes/RC-03-react-bootstrap/src/components/MyCard.jsx
@@ -13,19 +13,21 @@ const MyCard = ({veri}) => {
 
   return (
     <Container>
-<Row>
-{veri.map((a)=>{
+<Row className="g-3 text-center" >
+{veri.map(({name,text,img,id})=>{
     //!arrow (map) süslü kullandığında return ister.reactta süslü koymayabilirsiniz, o zaman returne de ihtiyaç olmaz
 
+//?database den çekilen veriler ekrana bastırılırken, en dış div unique bir veri ister bunu da key={id} şeklinde yazarız. id olmak zorunda değil unique herhangi bir property olabilir, mesela img
+
 return (
-  <Col>
+  <Col className="d-flex justify-content-center col-sm-12 col-md-6 col-lg-4" key={id} >
     <Card style={{ width: "18rem" }}>
-      <Card.Img variant="top" src={a.img} />
+      <Card.Img variant="top" src={img} />
       <Card.Body>
-        <Card.Title>{a.name}</Card.Title>
+        <Card.Title>{name}</Card.Title>
         <Card.Text>
        
-       {a.text}
+       {text}
         </Card.Text>
         <Button variant="primary">Go somewhere</Button>
       </Card.Body>
