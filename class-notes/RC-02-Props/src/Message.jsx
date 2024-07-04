2cc175fbd8fda527900535f0e81c785a85e8108a
rngnnn <rngnnnclk@gmail.com>
2024-07-04 10:33:31 +0200

    first commit

---
+++ class-notes/RC-02-Props/src/Message.jsx
@@ -0,0 +1,15 @@
+
+
+const Message = ({isim,telefon}) => {
+// console.log(props);
+
+
+  return (
+    <div style={{color:"red"}}>
+        <h1> merhaba benim adım {isim}</h1>
+<h2> telefon numaram {telefon}</h2>
+    </div>
+  )
+}
+
+export default Message
---
+++ class-notes/RC-03-react-bootstrap/src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+
+import"bootstrap/dist/css/bootstrap.min.css"
+import MyNavbar from "./components/MyNavbar";
+
+function App() {
+  return (
+    <div >
+ <MyNavbar/>
+    </div>
+  );
+}
+
+export default App;
